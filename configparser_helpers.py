be58432ad73f8f296d20fd497b55b4a8ec753fc1
Łukasz Langa <lukasz@langa.pl>
2011-04-29 13:18:53 +0200

    Got rid of the reprlib dependency.
    --HG--
    branch : 3.2-clean

--- configparser_helpers.py
+++ configparser_helpers.py
@@ -2,7 +2,37 @@
 # -*- coding: utf-8 -*-
 
 from collections import MutableMapping
-from reprlib import recursive_repr as _recursive_repr
+try:
+    from _thread import get_ident
+except ImportError:
+    from _dummy_thread import get_ident
+
+# from reprlib 3.2.1
+def recursive_repr(fillvalue='...'):
+    'Decorator to make a repr function return fillvalue for a recursive call'
+
+    def decorating_function(user_function):
+        repr_running = set()
+
+        def wrapper(self):
+            key = id(self), get_ident()
+            if key in repr_running:
+                return fillvalue
+            repr_running.add(key)
+            try:
+                result = user_function(self)
+            finally:
+                repr_running.discard(key)
+            return result
+
+        # Can't use functools.wraps() here because of bootstrap issues
+        wrapper.__module__ = getattr(user_function, '__module__')
+        wrapper.__doc__ = getattr(user_function, '__doc__')
+        wrapper.__name__ = getattr(user_function, '__name__')
+        wrapper.__annotations__ = getattr(user_function, '__annotations__', {})
+        return wrapper
+
+    return decorating_function
 
 # from collections 3.2.1
 class _ChainMap(MutableMapping):
@@ -48,7 +78,7 @@ class _ChainMap(MutableMapping):
     def __contains__(self, key):
         return any(key in m for m in self.maps)
 
-    @_recursive_repr()
+    @recursive_repr()
     def __repr__(self):
         return '{0.__class__.__name__}({1})'.format(
             self, ', '.join(map(repr, self.maps)))
